c1e93acee19039cc290a883771d7e7bea456f9d8
Fabien Girard <fabien@mapotempo.com>
2018-03-19 14:47:02 +0100

    Fix tests

--- config/initializers/without_callback.rb
+++ config/initializers/without_callback.rb
@@ -3,15 +3,18 @@ require 'set'
 module ActiveSupport::Callbacks::ClassMethods
   def without_callback(*args, &block)
     # Get all Filters (meaning all callback names as symboles)
-    filters_array = self.get_callbacks(args[0])
+    filters = self.get_callbacks(args[0])
                         .map(&:filter)
 
-    unless filters_array.to_set.superset?(args[1..args.length].to_set)
+    unless filters.to_set.superset?(args[1..args.length].to_set)
       raise Exception.new("Attempt to suppress a non existing callback") if Rails.env.development?
     end
 
-    skip_callback(*args)
-    yield
-    set_callback(*args)
+    begin
+      skip_callback(*args)
+      yield
+    ensure
+      set_callback(*args)
+    end
   end
 end
