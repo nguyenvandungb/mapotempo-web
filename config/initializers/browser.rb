8f577041e54f0ee7c304a086fcad58db3dffe9a5
Fabien Girard <fabien@mapotempo.com>
2018-04-26 09:50:46 +0200

    Fix send csv inline

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,3 +1,3 @@
 Rails.configuration.middleware.use Browser::Middleware do
-  redirect_to unsupported_browser_path(browser: :modern) if !browser.modern? && !request.env['PATH_INFO'].start_with?('/api/') && !request.env['QUERY_STRING'].include?('disposition=inline')
+  redirect_to unsupported_browser_path(browser: :modern) if !browser.modern? && !request.env['PATH_INFO'].start_with?('/api/') && (!request.env['QUERY_STRING'] || !request.env['QUERY_STRING'].include?('disposition=inline'))
 end
