8a86a610d973bb6d84718b100e3798f44440b037
fab-girard <fab.girard@gmail.com>
2019-02-14 22:24:12 +0100

    Improve error

--- app/models/types/schedule_type.rb
+++ app/models/types/schedule_type.rb
@@ -3,13 +3,13 @@ class ScheduleType < ActiveRecord::Type::Integer
     if value.kind_of?(Time) || value.kind_of?(DateTime)
       value.seconds_since_midnight.to_i
     elsif value.kind_of?(String)
-      raise ArgumentError.new(I18n.t('schedule_type.invalid')) if value !~ /\A[0-9: ]*\Z/
+      raise ArgumentError.new(I18n.t('schedule_type.invalid') + " #{value}") if value !~ /\A[0-9: ]*\Z/
 
       return nil if value.strip.empty?
       value += ':00' if value =~ /\A\d+:\d+\Z/
       ChronicDuration.parse(value, keep_zero: true)
     elsif value.kind_of?(Float) || value.kind_of?(ActiveSupport::Duration)
-      raise ArgumentError.new(I18n.t('schedule_type.invalid')) if value && value.to_i < 0
+      raise ArgumentError.new(I18n.t('schedule_type.invalid') + " #{value}") if value && value.to_i < 0
 
       value.to_i
     else
